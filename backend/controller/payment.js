b77b7a4883d87fd0ca595ea751395da0b1c60dcf
syed bilal haider <bilaldev151214@gmail.com>
2024-09-01 03:59:04 -0700

    update text details

--- backend/controller/payment.js
+++ backend/controller/payment.js
@@ -11,7 +11,7 @@ router.post(
       amount: req.body.amount,
       currency: "inr",
       metadata: {
-        company: "Becodemy",
+        company: "Ecommerce",
       },
     });
     res.status(200).json({
